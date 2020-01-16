d08c0444992dd9ecf7d53f416f28fccdfc6cbc97
Amelia Brandt <10827934@my.uvu.edu>
2020-01-15 17:17:39 -0700

    Messing With Variables

--- Farmer Vs Aliens/Assets/Scripts/Assignments/Variables/Variables.cs
+++ Farmer Vs Aliens/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Declaration
+    public float amount;
+    public float money;
+    public float cost;
+
+
+
+
+    // Start is called before the first fraime update
     void Start()
     {
-        
+        Cookies(amount, money, cost);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Cookies(float amount, float money, float cost)
+    {
+        float total;
+
+        total = money - amount * cost;
+
+        Debug.Log(total);
     }
 }
