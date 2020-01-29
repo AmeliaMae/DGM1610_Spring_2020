6c1dd41b5a4341a3718293b9cce8f9ac623474d9
Amelia Brandt <10827934@my.uvu.edu>
2020-01-29 16:58:49 -0700

    Gave A cube some movement controls and gave obstacles some tags

--- Farmer Vs Aliens/Assets/Scripts/Assignments/Variables/Move.cs
+++ Farmer Vs Aliens/Assets/Scripts/Assignments/Variables/Move.cs
@@ -4,26 +4,51 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed;
+    public float turnSpeed;
+    public float verticalInput;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0,0.1f);
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
     }
 
+}
+
+
     // Detect collison with another object
-     void OnCollisionEnter(Collision other)
+    /* void OnCollisionEnter(Collision other)
     {
-       
+          if (other.gameObject.CompareTag("Floor"))//Primary
+            {
+            Debug.Log("Colliding with Floor");
+            }
+        else if(other.gameObject.CompareTag("Obstacle"))//Secondary
+        {
+            Debug.Log("Colliding with Obstacle");
+        }
+        else//Default
+        {
+            Debug.Log("...");
+        }
     }
 
      void OnTriggerEnter(Collider other)
     {
-       
+        Debug.Log("You have entered the trigger!");
     }
 }
+*/
